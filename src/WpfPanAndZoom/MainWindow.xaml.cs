444e87b7e807b60d1cda48b82532ab2cff71d6de
Edoardo Inamo <avventureweb@gmail.com>
2024-10-31 12:51:33 +0100

    Add WpfPanAndZoom project

---
+++ src/WpfPanAndZoom/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using WpfPanAndZoom.CustomControls;
+
+namespace WpfPanAndZoom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ILogger<MainWindow> _logger;
+
+        public MainWindow(ILogger<MainWindow> logger)
+        {
+            _logger = logger;
+
+            InitializeComponent();
+
+            _logger.LogInformation("Components initialized");
+
+            LoadControls();
+        }
+
+        private void LoadControls()
+        {
+            Widget w1 = new Widget
+            {
+                Width = 200,
+                Height = 150
+            };
+            canvas.Children.Add(w1);
+            w1.Header.Text = "Widget 1";
+            Canvas.SetTop(w1, 100);
+            Canvas.SetLeft(w1, 100);
+
+            Widget w2 = new Widget
+            {
+                Width = 200,
+                Height = 150
+            };
+            canvas.Children.Add(w2);
+            w2.Header.Text = "Widget 2";
+            w2.HeaderRectangle.Fill = Brushes.Blue;
+            Canvas.SetTop(w2, 400);
+            Canvas.SetLeft(w2, 400);
+
+            Widget w3 = new Widget
+            {
+                Width = 200,
+                Height = 150
+            };
+            canvas.Children.Add(w3);
+            w3.Header.Text = "Widget 3";
+            w3.HeaderRectangle.Fill = Brushes.Red;
+            Canvas.SetTop(w3, 400);
+            Canvas.SetLeft(w3, 800);
+
+            _logger.LogInformation("Controls inserted");
+        }
+    }
+}
---
+++ src/WpfPanAndZoom/README.md
@@ -0,0 +1,6 @@
+# WpfPanAndZoom
+
+This project is about a control derived from Canvas with zoom and pan features.
+
+It has been ported to .NET 8 from this project: [WpfPanAndZoom](https://github.com/SEilers/WpfPanAndZoom) by [Sönke Eilers](https://github.com/SEilers).
+
