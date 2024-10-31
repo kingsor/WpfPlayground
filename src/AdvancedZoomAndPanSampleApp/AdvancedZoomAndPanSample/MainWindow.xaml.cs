19f8c7543b1c6a23d5b0ee199692fbb05db3ae34
Edoardo Inamo <avventureweb@gmail.com>
2024-10-31 19:39:22 +0100

    Add projects
    - AdvancedZoomAndPanSample
    - ZoomAndPan

---
+++ src/AdvancedZoomAndPanSampleApp/AdvancedZoomAndPanSample/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+using ZoomAndPanSample.ViewModels;
+
+namespace ZoomAndPanSample
+{
+    /// <summary>
+    ///     This is a Window that uses ZoomAndPanControl to zoom and pan around some content.
+    ///     This demonstrates how to use application specific mouse handling logic with ZoomAndPanControl.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+ 
+        public MainWindow()
+        {
+            //Rectangles = new ObservableCollection<Tuple<Rect, Color>>
+            //{
+            //    new Tuple<Rect, Color>(new Rect(50,25,50,25), Colors.Blue ),
+            //    new Tuple<Rect, Color>(new Rect(150,100,100,50), Colors.Aqua ),
+            //};
+            //DataContext = this;
+            InitializeComponent();
+
+            //ZoomAndPanControlView.rectCanvas.Rectangles = new ObservableCollection<Tuple<Rect, Color>>
+            //{
+            //    new Tuple<Rect, Color>(new Rect(50,25,50,25), Colors.Yellow ),
+            //    new Tuple<Rect, Color>(new Rect(150,100,100,50), Colors.Yellow ),
+            //};
+
+        }
+
+        /// <summary>
+        ///     Event raised when the Window has loaded.
+        /// </summary>
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //var helpTextWindow = new HelpTextWindow
+            //{
+            //    Left = Left + Width + 5,
+            //    Top = Top,
+            //    Owner = this
+            //};
+            //helpTextWindow.Show();
+        }
+
+
+        //public ObservableCollection<Tuple<Rect, Color>> Rectangles { get;
+        //    private set; }
+    }
+}
---
+++ src/AdvancedZoomAndPanSampleApp/AdvancedZoomAndPanSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace ZoomAndPanSample.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Tuple<Rect, Color>> Rectangles { get; set; }
+
+        public MainViewModel()
+        {
+            Rectangles = new ObservableCollection<Tuple<Rect, Color>>
+            {
+                new Tuple<Rect, Color>(new Rect(50,25,50,25), Colors.Blue ),
+                new Tuple<Rect, Color>(new Rect(150,100,100,50), Colors.Aqua ),
+            };
+        }
+    }
+}
