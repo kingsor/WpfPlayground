6fa8e0028239bb94aed20a47193a2bd26c9fdf26
Edoardo Inamo <avventureweb@gmail.com>
2024-11-21 09:48:41 +0100

    Add DragDropSample project

---
+++ src/DragDropSample/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DragDropSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private Point BasePoint = new Point(0.0, 0.0);
+        private double DeltaX = 0.0;
+        private double DeltaY = 0.0;
+        private bool moving = false;
+        private Point PositionInLabel;
+
+        public double XPosition
+        {
+            get { return BasePoint.X + DeltaX; }
+        }
+
+        public double YPosition
+        {
+            get { return BasePoint.Y + DeltaY; }
+        }
+
+        private void Feast_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Label l = e.Source as Label;
+            if (l != null)
+            {
+                l.CaptureMouse();
+                moving = true;
+                PositionInLabel = e.GetPosition(l);
+            }
+        }
+
+        private void Feast_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving)
+            {
+                Point p = e.GetPosition(MainCanvas);
+                DeltaX = p.X - BasePoint.X - PositionInLabel.X;
+                DeltaY = p.Y - BasePoint.Y - PositionInLabel.Y;
+                RaisePropertyChanged("XPosition");
+                RaisePropertyChanged("YPosition");
+            }
+        }
+
+        private void Feast_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Label l = e.Source as Label;
+            if (l != null)
+            {
+                l.ReleaseMouseCapture();
+                BasePoint.X += DeltaX;
+                BasePoint.Y += DeltaY;
+                DeltaX = 0.0;
+                DeltaY = 0.0;
+                moving = false;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private void StackPanel_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("Object"))
+            {
+                // These Effects values are used in the drag source's
+                // GiveFeedback event handler to determine which cursor to display.
+                if (e.KeyStates == DragDropKeyStates.ControlKey)
+                {
+                    e.Effects = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effects = DragDropEffects.Move;
+                }
+            }
+        }
+
+        private void StackPanel_Drop(object sender, DragEventArgs e)
+        {
+            // If an element in the panel has already handled the drop,
+            // the panel should not also handle it.
+            if (e.Handled == false)
+            {
+                Panel _panel = (Panel)sender;
+                UIElement _element = (UIElement)e.Data.GetData("Object");
+
+                if (_panel != null && _element != null)
+                {
+                    // Get the panel that the element currently belongs to,
+                    // then remove it from that panel and add it the Children of
+                    // the panel that its been dropped on.
+                    Panel _parent = (Panel)VisualTreeHelper.GetParent(_element);
+
+                    if (_parent != null)
+                    {
+                        if (e.KeyStates == DragDropKeyStates.ControlKey &&
+                            e.AllowedEffects.HasFlag(DragDropEffects.Copy))
+                        {
+                            Circle _circle = new Circle((Circle)_element);
+                            _panel.Children.Add(_circle);
+                            // set the value to return to the DoDragDrop call
+                            e.Effects = DragDropEffects.Copy;
+                        }
+                        else if (e.AllowedEffects.HasFlag(DragDropEffects.Move))
+                        {
+                            _parent.Children.Remove(_element);
+                            _panel.Children.Add(_element);
+                            // set the value to return to the DoDragDrop call
+                            e.Effects = DragDropEffects.Move;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/DragDropSample/README.md
@@ -0,0 +1,18 @@
+# DragDropSample
+
+This demo project is a way to learn how to use drag and drop.
+
+Starting from this post:
+- [Walkthrough: Enabling Drag and Drop on a User Control - WPF .NET Framework | Microsoft Learn](https://learn.microsoft.com/en-us/dotnet/desktop/wpf/advanced/walkthrough-enabling-drag-and-drop-on-a-user-control?view=netframeworkdesktop-4.8)
+
+In the same project there is a test of:
+- the slider control and RenderTransform property.
+    - [#773 – A Rotation Center Point Can Be Outside an Element](https://wpf.2000things.com/2013/03/11/773-a-rotation-center-point-can-be-outside-an-element/)
+    - [The Slider control](https://wpf-tutorial.com/misc-controls/the-slider-control/)
+- moving an element with the mouse in a canvas
+    - [#688 – Moving an Element with the Mouse](https://wpf.2000things.com/2012/11/12/688-moving-an-element-with-the-mouse/)
+
+
+
+
+
